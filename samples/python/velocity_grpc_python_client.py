d4696a46beef4962d13e8c4082701d98e879616d
Yujing Wu <90715031+yujingwu1997@users.noreply.github.com>
2021-11-02 16:53:10 -0400

    update to add asynchronous implementation of send

--- samples/python/velocity_grpc_python_client.py
+++ samples/python/velocity_grpc_python_client.py
@@ -1,4 +1,5 @@
 '''
+blocking, asynchronous, flow-control 
 This script implements a gRPC client in Python. 
 Before sending data using this client, a gRPC feed needs to be created on Velocity. 
 The schema of the feed (data types & order) should match exactly the schema of the data sent from this client. 
@@ -10,59 +11,139 @@ import velocity_grpc_pb2_grpc
 from google.protobuf.any_pb2 import Any
 from google.protobuf import wrappers_pb2 as wrappers
 import logging
+import time
 
-def send_velocity_features(stub):
+SCHEMA = ['Float64', 'Float64', 'String']
+PATH_TO_CER = 'ISRG Root X1.pem'
+SERVER_URL = 'a4iot-a4iotdev-c2.westus2.cloudapp.azure.com'
+SERVER_HEARDER = 'a4iotdev.89bb713384f146f89d73af23b7722dab'
+
+def data_collection():
     '''
-    Define a function to create a request and send it to the gRPC feed created on Velocity
-    This request contains one feature with three attributes 
+    Read data and translate it to a 2D array 
     '''
-    # create a list that contains the attributes of the event 
-    # make sure the attributes are wrapped to match the data types listed in the schema on Velocity feed
-    attributes = [
-        wrappers.FloatValue(value=37.592424978605884),
-        wrappers.FloatValue(value=-92.76911615610153),
-        wrappers.StringValue(value='test_loc_0')
-        ]
+    # create a 2D array that contains the data to be sent 
+    # the order and data types should match the schema specified on Velocity
+    df = [[39.29242438926388, -76.6666720609419, '9:17AM'],
+    [35.29242438926388, -70.6666720609419, '1:38PM'],
+    [39.160776580893554, -77.30070336032382, '8:57AM']]
+
+    return df 
+
+def wrap_data(df):
+    '''
+    Wrap data to ensure the data types match 100% the schema on Velocity
+    '''
+    df_copy = df.copy()
+    for data in df_copy:
+        for i in range(0, len(SCHEMA)):
+            if SCHEMA[i] == 'Float64':
+                data[i] = wrappers.FloatValue(value=data[i])
+            elif SCHEMA[i] == 'String':
+                data[i] = wrappers.StringValue(value=data[i])
+
+    return df_copy
+
+def data_to_feature(df):
+    '''
+    Build features for requests from data 
+    '''
+    features = []
+    for data in df: 
+        # create a message of Any message type 
+        any = Any()
+        # create a feature 
+        feature = velocity_grpc_pb2.Feature()
+        
+        for attribute in data:
+            # pack the attribute into the message 
+            any.Pack(attribute)
+            # append the message to the empty feature so we now have a feature to send 
+            feature.attributes.append(any)
+
+        features.append(feature)
     
-    # create a message of Any message type 
-    any = Any()
-    # create a feature 
-    test_feature = velocity_grpc_pb2.Feature()
+    return features
 
-    # loop through the list of attributes
-    for attri in attributes:
-        # pack the attribute into the message 
-        any.Pack(attri)
-        # append the message to the empty feature so we now have a feature to send 
-        test_feature.attributes.append(any)
-
-    # specify the gRPC endpoint header path that can be found on the details page of the created feed on Velocity
-    metadata = (('grpc-path', 'a4iotdev.978868e6b2254f579fa95baa35fbc431'),)
-
-    # send the request containing the feature we created
-    res = stub.send(request = velocity_grpc_pb2.Request(features=[test_feature]), metadata=metadata)
-
-    return res
-
-def run():
+def create_channel():
 
     # read the root certificate from file
     # the certificate can be obtained from https://letsencrypt.org/certificates/
-    with open('ISRG Root X1.pem', 'rb') as f:
+    with open(PATH_TO_CER, 'rb') as f:
         trusted_certs = f.read()
     # create credentials for the channel based on the root certificate 
     creds = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
 
     # create the channel using the gRPC endpoint URL
-    with grpc.secure_channel('a4iot-a4iotdev-c2.westus2.cloudapp.azure.com', creds) as channel:
+    channel = grpc.secure_channel(SERVER_URL, creds)
+
+    return channel
+
+def send_blocking(stub, metadata, features):
+    '''
+    Send blocking requests to Velocity
+    Each request contains one single feature 
+    '''
+    # send the request containing the feature we created
+    for feature in features:
+        res = stub.send(request = velocity_grpc_pb2.Request(features=[feature]), metadata=metadata)
+        # print out the response 
+        logging.info(res)
+
+def process_async_responses(future):
+    # log the result of an asynchronous request
+    print(future.result())
+
+def send_asynchronous(stub, metadata, features):
+    futures = []
+    # loop through all features
+    for feature in features:
+        # send each feature via an asynchronous request 
+        # this is non-blocking 
+        call_future = stub.send.future(request = velocity_grpc_pb2.Request(features=[feature]), metadata=metadata)
+
+        call_future.add_done_callback(process_async_responses)
+
+        futures.append(call_future)
+
+    return futures 
+
+
+
+
+def main():
+        data = data_collection()
+        wrapped_data = wrap_data(data)
+        features = data_to_feature(wrapped_data)
+        
+        # send the request via the stub 
+        channel = create_channel()
+
+        # specify the gRPC endpoint header path that can be found on the details page of the created feed on Velocity
+        metadata = (('grpc-path', SERVER_HEARDER),)
+ 
         # create a stub with the channel
         stub = velocity_grpc_pb2_grpc.GrpcFeedStub(channel)
-        # send the request via the stub 
-        response = send_velocity_features(stub)
-        # print out the response 
-        logging.info(response)
 
+        # send the features to Velocity via blocking requests 
+        # i.e. a request is sent after the response from the server is received for the previous request 
+        #send_blocking(stub=stub, metadata=metadata,features=features)
+
+        # send the features to Velocity via asynchronous requests 
+        futures = send_asynchronous(stub=stub, metadata=metadata,features=features)
+        
+        futures_completed = False 
+        
+        while futures_completed != True:
+            time.sleep(10)
+            futures_completed = True
+            for future in futures:
+                if future.done():
+                    continue
+                else: 
+                    futures_completed = False
+                    print("Requests still not completed")
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    main()
