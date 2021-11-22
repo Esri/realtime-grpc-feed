bbc85a78cd9980f52314a1c6afba5a747cd28fcc
Ken Gorton <kgorton@esri.com>
2021-11-22 11:59:51 -0500

    Update Program.cs

--- samples/csharp/grpc-sender/Program.cs
+++ samples/csharp/grpc-sender/Program.cs
@@ -16,13 +16,11 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
