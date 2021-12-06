6c25072bfcf34b5fa2fcc981e5d34081142581dd
Ken Gorton <kgorton@esri.com>
2021-12-06 12:37:37 -0500

    Update Program.cs

--- samples/csharp/Simple gRPC Client/GrpcVelocityClient/Program.cs
+++ samples/csharp/Simple gRPC Client/GrpcVelocityClient/Program.cs
@@ -39,7 +39,7 @@ string gRPC_endpoint_header_path_key = "grpc-path";
 int gRPC_endpoint_URL_port = 443;
 
 //data to send
-string jsonDataString = "[{\"lat\":39.29242438926388,\"lon\":-76.6666720609419,\"name\":\"Evan\",\"active\":false,\"id\":4,\"timestamp\":1636384539000},{\"lat\":38.29242438926388,\"lon\":-74.6666720609419,\"name\":\"Brody\",\"active\":true,\"id\":1,\"timestamp\":1636384599000},{\"lat\":35.29242438926388,\"lon\":-70.6666720609419,\"name\":\"Sarah\",\"active\":false,\"id\":2,\"timestamp\":1636384649000},{\"lat\":39.16077658089355,\"lon\":-77.3007033603238,\"name\":\"Cortney\",\"active\":true,\"id\":3,\"timestamp\":1636384709000}]";
+string jsonDataString = "[{\"lat\":39.29242438926388,\"lon\":-76.6666720609419,\"name\":\"Evan\",\"active\":false,\"id\":4,\"timestamp\":1636384539000},{\"lat\":38.905809,\"lon\":-77.091489,\"name\":\"Brody\",\"active\":true,\"id\":1,\"timestamp\":1636384599000},{\"lat\":38.580191,\"lon\":-77.421078,\"name\":\"Sarah\",\"active\":false,\"id\":2,\"timestamp\":1636384649000},{\"lat\":39.16077658089355,\"lon\":-77.3007033603238,\"name\":\"Cortney\",\"active\":true,\"id\":3,\"timestamp\":1636384709000}]";
 
 dynamic jsonData = JsonConvert.DeserializeObject<JArray>(jsonDataString);
 
